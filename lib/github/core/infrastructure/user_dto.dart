4591e21957f753dcbd0a61a19b1bd2daa55bcde7
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-29 18:42:30 +0200

    added user dto

---
+++ lib/github/core/infrastructure/user_dto.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/core/domain/user.dart';
+part 'user_dto.freezed.dart';
+part 'user_dto.g.dart';
+
+@freezed
+class UserDTO with _$UserDTO {
+  const factory UserDTO({
+    @JsonKey(name: 'login') required String name,
+    @JsonKey(name: 'avatar_url') required String avatarUrl,
+  }) = _UserDTO;
+
+  factory UserDTO.fromJson(Map<String, dynamic> json) =>
+      _$UserDTOFromJson(json);
+
+  factory UserDTO.fromDomain(User user) {
+    return UserDTO(name: user.name, avatarUrl: user.avatarUrl);
+  }
+
+  User toDomain() {
+    return User(name: name, avatarUrl: avatarUrl);
+  }
+}
