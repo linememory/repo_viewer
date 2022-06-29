8632496cef1e9c80c46b9d53ebd644fc53609a04
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-29 18:42:44 +0200

    added github repo dto

---
+++ lib/github/core/infrastructure/github_repo_dto.dart
@@ -0,0 +1,43 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/core/infrastructure/user_dto.dart';
+part 'github_repo_dto.freezed.dart';
+part 'github_repo_dto.g.dart';
+
+// String _descriptionFromJson(Object? value) {
+//   return (value as String?) ?? '';
+// }
+
+@freezed
+class GithubRepoDTO with _$GithubRepoDTO {
+  const factory GithubRepoDTO({
+    required UserDTO owner,
+    @JsonKey(name: 'name')
+        required String name,
+    @JsonKey(name: 'description', defaultValue: '') //, fromJson: _descriptionFromJson)
+        required String description,
+    @JsonKey(name: 'stargazers_count')
+        required int stargazersCount,
+  }) = _GithubRepoDTO;
+
+  factory GithubRepoDTO.fromJson(Map<String, dynamic> json) =>
+      _$GithubRepoDTOFromJson(json);
+
+  factory GithubRepoDTO.fromDomain(GithubRepo repo) {
+    return GithubRepoDTO(
+      owner: UserDTO.fromDomain(repo.owner),
+      name: repo.name,
+      description: repo.description,
+      stargazersCount: repo.stargazersCount,
+    );
+  }
+
+  GithubRepo toDomain() {
+    return GithubRepo(
+      owner: owner.toDomain(),
+      name: name,
+      description: description,
+      stargazersCount: stargazersCount,
+    );
+  }
+}
