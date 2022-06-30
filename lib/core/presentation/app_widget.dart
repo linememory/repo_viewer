714979bb27a8fdb0b26deeb909eecff784a05561
Lukas Leitner <lukasleitner17@gmail.com>
2022-06-30 10:50:30 +0200

    added sembast provider and initialize sembast database

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -4,8 +4,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repo_viewer/auth/application/auth_notifier.dart';
 import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
+import 'package:repo_viewer/core/shared/providers.dart';
 
 final initializationProvider = FutureProvider((ref) async {
+  await ref.read(sembastProvider).init();
   await Future.delayed(const Duration(seconds: 1));
   await ref.read(authNotifierProvider.notifier).checkAndUpdateAuthStatus();
   return unit;
