83e7af7a223cc17c534869d3ea787c61133cc851
Lukas Leitner <lukasleitner17@gmail.com>
2022-07-08 15:56:00 +0200

    add search history repository

---
+++ lib/search/infrastructure/search_history_repository.dart
@@ -0,0 +1,76 @@
+import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
+import 'package:sembast/sembast.dart';
+
+class SearchHistoryRepository {
+  final SembastDatabase _sembastDatabase;
+  final _store = StoreRef<int, String>('searchHistory');
+
+  SearchHistoryRepository(this._sembastDatabase);
+
+  static const historyLength = 10;
+
+  Stream<List<String>> watchSearchTerms({String? filter}) {
+    return _store
+        .query(
+          finder: filter != null && filter.isNotEmpty
+              ? Finder(
+                  filter: Filter.custom(
+                    (record) => (record.value as String).startsWith(filter),
+                  ),
+                )
+              : null,
+        )
+        .onSnapshots(_sembastDatabase.instance)
+        .map(
+          (record) =>
+              record.reversed.map((snapshot) => snapshot.value).toList(),
+        );
+  }
+
+  Future<void> addSearchTerm(String term) async =>
+      _addSearchTerm(term, _sembastDatabase.instance);
+
+  Future<void> deleteSearchTerm(String term) async =>
+      _deleteSearchTerm(term, _sembastDatabase.instance);
+
+  Future<void> putSearchTermFirst(String term) async {
+    _sembastDatabase.instance.transaction((transaction) async {
+      await _deleteSearchTerm(term, transaction);
+      await _addSearchTerm(term, transaction);
+    });
+  }
+
+  Future<void> _addSearchTerm(String term, DatabaseClient dbClient) async {
+    final existingKey = await _store.findKey(
+      dbClient,
+      finder: Finder(
+        filter: Filter.custom(
+          (record) => record.value == term,
+        ),
+      ),
+    );
+    if (existingKey != null) {
+      putSearchTermFirst(term);
+    } else {
+      await _store.add(dbClient, term);
+      final count = await _store.count(dbClient);
+      if (count > historyLength) {
+        _store.delete(
+          dbClient,
+          finder: Finder(limit: count - historyLength),
+        );
+      }
+    }
+  }
+
+  Future<void> _deleteSearchTerm(String term, DatabaseClient dbClient) async {
+    await _store.delete(
+      dbClient,
+      finder: Finder(
+        filter: Filter.custom(
+          (record) => record.value == term,
+        ),
+      ),
+    );
+  }
+}
