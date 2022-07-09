692aa9cb2a63ede26c99afdaf74a87ce47d78a96
Lukas Leitner <lukasleitner17@gmail.com>
2022-07-09 18:12:52 +0200

    add github repo detail dto

---
+++ lib/github/detail/infrastructure/github_repo_detail_dto.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/detail/domain/github_repo_detail.dart';
+
+part 'github_repo_detail_dto.freezed.dart';
+part 'github_repo_detail_dto.g.dart';
+
+@freezed
+class GithubRepoDetailDTO with _$GithubRepoDetailDTO {
+  const factory GithubRepoDetailDTO({
+    required String fullName,
+    required String html,
+    required bool starred,
+  }) = _GithubRepoDetailDTO;
+
+  factory GithubRepoDetailDTO.fromJson(Map<String, dynamic> json) =>
+      _$GithubRepoDetailDTOFromJson(json);
+
+  GithubRepoDetail toDomain() => GithubRepoDetail(
+        fullName: fullName,
+        html: html,
+        starred: starred,
+      );
+}
